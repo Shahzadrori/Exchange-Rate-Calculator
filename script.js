dcf9be744c7406adea5993e7d6125aa7dcc88d49
Shahzad <shahzadrori@gmail.com>
2020-11-25 00:35:47 -0800

    correcting the wrong written words

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ function calculate() {
   const currency_two = currencyEl_two.value;
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
   .then(res => res.json())
-  .then(data => {const rate = data.rates[currency_two];
-    rateEld.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+  .then(data => {
+    const rate = data.rates[currency_two];
+    rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
     amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
 
   })
