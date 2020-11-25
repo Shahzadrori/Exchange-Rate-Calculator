eba6d6fe4443af86a13314a6b58f7e2e6f7994b8
Shahzad <shahzadrori@gmail.com>
2020-11-25 00:28:25 -0800

    adding up fetch command

--- script.js
+++ script.js
@@ -6,10 +6,17 @@ const swapEl = document.getElementById("swap");
 const rateEl = document.getElementById("rate");
 
 //fetch and update DOM
+
 function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
-  fetch(`https://api.exchangerate-api.com/v4/latest/USD`);
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+  .then(res => res.json())
+  .then(data => {const rate = data.rates[currency_two];
+    rateEld.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+    amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+
+  })
 }
 //add event listner
 currencyEl_one.addEventListener("change", calculate);
